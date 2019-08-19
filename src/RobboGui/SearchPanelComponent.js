be5d0eaf7bd9c2dfff7feab0c1038469122f9e64
Yarila682 <Yarila682@yandex.ru>
2019-08-19 05:19:40 +0300

    uncomment smth

--- src/RobboGui/SearchPanelComponent.js
+++ src/RobboGui/SearchPanelComponent.js
@@ -285,12 +285,12 @@ class SearchPanelComponent extends Component {
 
           {
 
-            ( (this.bluetooth_devices_state == "searching") /*&& (node_process.platform === "win32")*/)?<div className={styles.bluetooth_devices_not_found}>{this.props.intl.formatMessage(messages.bluetooth_searching)}</div>:""
+            ( (this.bluetooth_devices_state == "searching") && (node_process.platform === "win32"))?<div className={styles.bluetooth_devices_not_found}>{this.props.intl.formatMessage(messages.bluetooth_searching)}</div>:""
 
           }
 
           {
-             ( (this.bluetooth_devices_state == "not_found") /*&& (node_process.platform === "win32")*/) ? <div className={styles.bluetooth_devices_not_found}>{this.props.intl.formatMessage(messages.bluetooth_devices_not_found)}</div>:""
+             ( (this.bluetooth_devices_state == "not_found") && (node_process.platform === "win32")) ? <div className={styles.bluetooth_devices_not_found}>{this.props.intl.formatMessage(messages.bluetooth_devices_not_found)}</div>:""
 
           }
 
