c77e244731d5c14f24eb75addfeaeeeaf3463219
Yarila <yarila682@yandex.ru>
2019-08-30 12:04:38 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -139,7 +139,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch 3.0 GUI',
+                title: 'Robbo Scratch 3',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
